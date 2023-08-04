5061f35b318ab0261f2ee1a47bf414b382e992f4
Bede Constantinides <bedeabc@gmail.com>
2023-08-04 14:47:42 +0100

    Move private functions into util.py

--- src/gpas_client/lib.py
+++ src/gpas_client/lib.py
@@ -1,10 +1,5 @@
-import csv
 import json
 import logging
-import random
-import string
-
-from urllib.parse import urlparse
 
 from pathlib import Path
 
@@ -12,28 +7,14 @@ import httpx
 
 from hostile.lib import clean_paired_fastqs
 
-from gpas_client.models import UploadBatch, UploadSample
-from gpas_client.util import hash_files
-
-
-def generate_identifier(length=6):
-    letters_and_digits = string.ascii_letters + string.digits
-    random_identifier = "".join(
-        random.choice(letters_and_digits) for _ in range(length)
-    )
-    return random_identifier.lower()
-
-
-def get_host_name(url) -> str:
-    parsed_uri = urlparse(url)
-    return parsed_uri.hostname
+from gpas_client import util
 
 
 def authenticate(
     username: str, password: str, host="https://dev.portal.gpas.world/api"
 ) -> None:
     """Requests, writes auth token to ~/.config/gpas/tokens/<host>"""
-    host_name = get_host_name(host)
+    host_name = util.get_host_name(host)
     logging.info(f"{username=} {password=}")
     response = httpx.post(
         f"{host}/v1/auth/token",
@@ -48,43 +29,12 @@ def authenticate(
         json.dump(data, fh)
 
 
-def get_access_token(host="https://dev.portal.gpas.world/api") -> str:
-    """Reads token from ~/.config/gpas/tokens/<host>"""
-    host_name = get_host_name(host)
-    token_path = Path.home() / ".config" / "gpas" / "tokens" / f"{host_name}.json"
-    logging.debug(f"{token_path=}")
-    data = json.loads((token_path).read_text())
-    return data["access_token"].strip()
-
-
-def parse_csv(csv_path: Path) -> list[dict]:
-    """Parse CSV returning a list of dictionaries"""
-    with open(csv_path, "r") as fh:
-        reader = csv.DictReader(fh)
-        return [row for row in reader]
-
-
-def write_csv(records: list[dict], file_name: Path) -> None:
-    """Write a list of dictionaries to a CSV file"""
-    with open(file_name, "w", newline="") as fh:
-        fieldnames = records[0].keys()
-        writer = csv.DictWriter(fh, fieldnames=fieldnames)
-        writer.writeheader()
-        for r in records:
-            writer.writerow(r)
-
-
-def parse_upload_csv(upload_csv: Path) -> UploadBatch:
-    records = parse_csv(upload_csv)
-    return UploadBatch(samples=[UploadSample(**r) for r in records])
-
-
 def create_batch(name: str) -> int:
     """Create batch on server, return batch id"""
     data = {"name": name, "telemetry_data": {}}
     response = httpx.post(
         "https://dev.portal.gpas.world/api/v1/batches",
-        headers={f"Authorization": f"Bearer {get_access_token()}"},
+        headers={f"Authorization": f"Bearer {util.get_access_token()}"},
         json=data,
     )
     if response.is_error:
@@ -129,7 +79,7 @@ def create_sample(
         "host_organism": host_organism,
         "checksum": checksum,
     }
-    headers = {f"Authorization": f"Bearer {get_access_token()}"}
+    headers = {f"Authorization": f"Bearer {util.get_access_token()}"}
     logging.debug(f"Sample {data=}")
     response = httpx.post(
         "https://dev.portal.gpas.world/api/v1/samples",
@@ -145,36 +95,9 @@ def create_sample(
     return response.json()["id"]
 
 
-def upload_sample_files(sample_id: int, reads_1: Path, reads_2: Path) -> None:
-    """Upload paired FASTQ files to server"""
-    reads_1, reads_2 = Path(reads_1), Path(reads_2)
-    with open(reads_1, "rb") as fh:
-        response1 = httpx.post(
-            f"https://dev.portal.gpas.world/api/v1/samples/{sample_id}/files",
-            headers={f"Authorization": f"Bearer {get_access_token()}"},
-            files={"file": fh},
-        )
-        if response1.status_code == httpx.codes.is_error:
-            try:
-                logging.error(json.dumps(response1.json(), indent=4))
-            finally:
-                response1.raise_for_status()
-    with open(reads_2, "rb") as fh:
-        response2 = httpx.post(
-            f"https://dev.portal.gpas.world/api/v1/samples/{sample_id}/files",
-            headers={f"Authorization": f"Bearer {get_access_token()}"},
-            files={"file": fh},
-        )
-        if response2.status_code == httpx.codes.is_error:
-            try:
-                logging.error(json.dumps(response2.json(), indent=4))
-            finally:
-                response2.raise_for_status()
-
-
 def patch_sample(sample_id: int):
     """Patch sample status to Ready"""
-    headers = {f"Authorization": f"Bearer {get_access_token()}"}
+    headers = {f"Authorization": f"Bearer {util.get_access_token()}"}
     response = httpx.patch(
         f"https://dev.portal.gpas.world/api/v1/samples/{sample_id}",
         headers=headers,
@@ -190,7 +113,7 @@ def patch_sample(sample_id: int):
 def upload(upload_csv: Path, dry_run: bool = False) -> None:
     """Upload a batch of one or more samples to the GPAS platform"""
     upload_csv = Path(upload_csv)
-    batch = parse_upload_csv(upload_csv)
+    batch = util.parse_upload_csv(upload_csv)
 
     fastq_path_tuples = [
         (upload_csv.parent / s.reads_1, upload_csv.parent / s.reads_2)
@@ -202,7 +125,7 @@ def upload(upload_csv: Path, dry_run: bool = False) -> None:
     logging.debug(f"{names_logs=}")
     control_map = {"positive": True, "negative": False, "": None}
     if not dry_run:
-        batch_name = generate_identifier()
+        batch_name = util.generate_identifier()
         batch_id = create_batch(batch_name)
         mapping_csv_records = []
         for sample in batch.samples:
@@ -221,7 +144,7 @@ def upload(upload_csv: Path, dry_run: bool = False) -> None:
                 ],
                 client_decontamination_reads_in=names_logs[name]["reads_in"],
                 client_decontamination_reads_out=names_logs[name]["reads_out"],
-                checksum=hash_files(reads_1_clean, reads_2_clean),
+                checksum=util.hash_files(reads_1_clean, reads_2_clean),
             )
             mapping_csv_records.append(
                 {
@@ -231,19 +154,18 @@ def upload(upload_csv: Path, dry_run: bool = False) -> None:
                     "remote_sample_name": sample_id,
                 }
             )
-            upload_sample_files(
+            util.upload_paired_fastqs(
                 sample_id=sample_id, reads_1=reads_1_clean, reads_2=reads_2_clean
             )
-
             logging.info(f"Uploaded {name}")
             # patch_sample(sample_id)
-        write_csv(mapping_csv_records, f"{batch_name}.mapping.csv")
+        util.write_csv(mapping_csv_records, f"{batch_name}.mapping.csv")
         logging.info(f"Uploaded batch {batch_name}")
 
 
 def list_batches():
     """List batches on server"""
-    headers = {f"Authorization": f"Bearer {get_access_token()}"}
+    headers = {f"Authorization": f"Bearer {util.get_access_token()}"}
     response = httpx.get(
         "https://dev.portal.gpas.world/api/v1/batches", headers=headers
     )
@@ -257,7 +179,7 @@ def list_batches():
 
 def list_samples() -> None:
     """List samples on server"""
-    headers = {f"Authorization": f"Bearer {get_access_token()}"}
+    headers = {f"Authorization": f"Bearer {util.get_access_token()}"}
     response = httpx.get(
         f"https://dev.portal.gpas.world/api/v1/samples",
         headers=headers,
@@ -272,7 +194,7 @@ def list_samples() -> None:
 
 def fetch_sample(sample_id: int):
     """Fetch sample data from server"""
-    headers = {f"Authorization": f"Bearer {get_access_token()}"}
+    headers = {f"Authorization": f"Bearer {util.get_access_token()}"}
     response = httpx.get(
         f"https://dev.portal.gpas.world/api/v1/samples/{sample_id}",
         headers=headers,
--- src/gpas_client/util.py
+++ src/gpas_client/util.py
@@ -1,6 +1,60 @@
+import csv
 import hashlib
+import json
+import logging
+import random
+import string
+
+import httpx
 
 from pathlib import Path
+from urllib.parse import urlparse
+
+from gpas_client.models import UploadBatch, UploadSample
+
+
+def generate_identifier(length=6):
+    letters_and_digits = string.ascii_letters + string.digits
+    random_identifier = "".join(
+        random.choice(letters_and_digits) for _ in range(length)
+    )
+    return random_identifier.lower()
+
+
+def get_host_name(url) -> str:
+    parsed_uri = urlparse(url)
+    return parsed_uri.hostname
+
+
+def get_access_token(host="https://dev.portal.gpas.world/api") -> str:
+    """Reads token from ~/.config/gpas/tokens/<host>"""
+    host_name = get_host_name(host)
+    token_path = Path.home() / ".config" / "gpas" / "tokens" / f"{host_name}.json"
+    logging.debug(f"{token_path=}")
+    data = json.loads((token_path).read_text())
+    return data["access_token"].strip()
+
+
+def parse_csv(csv_path: Path) -> list[dict]:
+    """Parse CSV returning a list of dictionaries"""
+    with open(csv_path, "r") as fh:
+        reader = csv.DictReader(fh)
+        return [row for row in reader]
+
+
+def parse_upload_csv(upload_csv: Path) -> UploadBatch:
+    records = parse_csv(upload_csv)
+    return UploadBatch(samples=[UploadSample(**r) for r in records])
+
+
+def write_csv(records: list[dict], file_name: Path | str) -> None:
+    """Write a list of dictionaries to a CSV file"""
+    with open(file_name, "w", newline="") as fh:
+        fieldnames = records[0].keys()
+        writer = csv.DictWriter(fh, fieldnames=fieldnames)
+        writer.writeheader()
+        for r in records:
+            writer.writerow(r)
 
 
 def hash_files(*paths: Path) -> str:
@@ -10,3 +64,30 @@ def hash_files(*paths: Path) -> str:
             for block in iter(lambda: f.read(16_384), b""):
                 hasher.update(block)
     return hasher.hexdigest()
+
+
+def upload_paired_fastqs(sample_id: int, reads_1: Path, reads_2: Path) -> None:
+    """Upload paired FASTQ files to server"""
+    reads_1, reads_2 = Path(reads_1), Path(reads_2)
+    with open(reads_1, "rb") as fh:
+        response1 = httpx.post(
+            f"https://dev.portal.gpas.world/api/v1/samples/{sample_id}/files",
+            headers={f"Authorization": f"Bearer {get_access_token()}"},
+            files={"file": fh},
+        )
+        if response1.status_code == httpx.codes.is_error:
+            try:
+                logging.error(json.dumps(response1.json(), indent=4))
+            finally:
+                response1.raise_for_status()
+    with open(reads_2, "rb") as fh:
+        response2 = httpx.post(
+            f"https://dev.portal.gpas.world/api/v1/samples/{sample_id}/files",
+            headers={f"Authorization": f"Bearer {get_access_token()}"},
+            files={"file": fh},
+        )
+        if response2.status_code == httpx.codes.is_error:
+            try:
+                logging.error(json.dumps(response2.json(), indent=4))
+            finally:
+                response2.raise_for_status()
