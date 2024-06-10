ec9eeac1fd370124e49b9b196ea7e6970b8f198e
bobturneruk <robert.turner@ndm.ox.ac.uk>
2024-06-10 11:54:32 +0000

    test: Correct warnings for mismatched fastqs

--- tests/test_lib.py
+++ tests/test_lib.py
@@ -2,12 +2,16 @@ from gpas import lib
 
 
 def test_fastq_gz_match(human_1_1_fastq_gz, human_1_2_fastq_gz):
-    assert lib.fastq_match(human_1_1_fastq_gz, human_1_2_fastq_gz)
+    assert lib.valid_fastq(human_1_1_fastq_gz, human_1_2_fastq_gz)
 
 
-def test_not_fastq_gz_match(bad_1_1_fastq_gz, human_1_2_fastq_gz):
-    assert not lib.fastq_match(bad_1_1_fastq_gz, human_1_2_fastq_gz)
+def test_not_fastq_gz_match(bad_1_1_fastq_gz, human_1_2_fastq_gz, caplog):
+    assert not lib.valid_fastq(bad_1_1_fastq_gz, human_1_2_fastq_gz)
+    assert (
+        "WARNING  root:lib.py:807 FASTQ files tests/data/reads/bad_1_1.fastq.gz and tests/data/reads/human_1_2.fastq.gz do not have the same number of lines"
+        in caplog.text
+    )
 
 
-def test_fastq_match(sars_cov_2_1_1_fastq, sars_cov_2_1_2_fastq):
-    assert lib.fastq_match(sars_cov_2_1_1_fastq, sars_cov_2_1_2_fastq)
+def test_valid_fastq(sars_cov_2_1_1_fastq, sars_cov_2_1_2_fastq):
+    assert lib.valid_fastq(sars_cov_2_1_1_fastq, sars_cov_2_1_2_fastq)
