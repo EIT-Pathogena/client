ffa18657f347e68aac639a8a24ff68b95915b7c3
Jack Alexander <jack.alexander@hippodigital.co.uk>
2024-10-27 16:41:15 +0000

    docs(test): Add docstrings for csv build tests

--- tests/test_build_csv.py
+++ tests/test_build_csv.py
@@ -10,6 +10,17 @@ from pathogena.create_upload_csv import build_upload_csv, UploadData
 
 
 def test_build_csv_illumina(tmp_path: Path, caplog: pytest.LogCaptureFixture, upload_data: UploadData) -> None:
+    """
+    Test building CSV for Illumina platform.
+
+    This test checks that the output illumina csv is the same as the temp output csv, that the csv creation
+    can be seen in the log output, and that some help text can be seen in the log output.
+
+    Args:
+        tmp_path (Path): Temporary path for output files.
+        caplog (pytest.LogCaptureFixture): Fixture to capture log output.
+        upload_data (UploadData): Data required for building the upload CSV.
+    """
     caplog.set_level(logging.INFO)
     build_upload_csv(
         "tests/data/empty_files",
@@ -29,6 +40,17 @@ def test_build_csv_illumina(tmp_path: Path, caplog: pytest.LogCaptureFixture, up
 
 
 def test_build_csv_ont(tmp_path: Path, caplog: pytest.LogCaptureFixture, upload_data: UploadData) -> None:
+    """
+    Test building CSV for ONT platform.
+
+    This test checks that the output ont csv is the same as the temp output csv, and that the csv creation
+    can be seen in the log output.
+
+    Args:
+        tmp_path (Path): Temporary path for output files.
+        caplog (pytest.LogCaptureFixture): Fixture to capture log output.
+        upload_data (UploadData): Data required for building the upload CSV.
+    """
     caplog.set_level(logging.INFO)
     upload_data.instrument_platform = "ont"
     upload_data.district = "dis"
@@ -47,6 +69,17 @@ def test_build_csv_ont(tmp_path: Path, caplog: pytest.LogCaptureFixture, upload_
 
 
 def test_build_csv_batches(tmp_path: Path, caplog: pytest.LogCaptureFixture, upload_data: UploadData) -> None:
+    """
+    Test building CSV in batches.
+
+    This test checks that the both of the batch csvs are the same as the temp output csvs, and that the csv creation
+    can be seen in the log output.
+
+    Args:
+        tmp_path (Path): Temporary path for output files.
+        caplog (pytest.LogCaptureFixture): Fixture to capture log output.
+        upload_data (UploadData): Data required for building the upload CSV.
+    """
     caplog.set_level(logging.INFO)
     upload_data.max_batch_size = 3
     build_upload_csv(
@@ -65,6 +98,16 @@ def test_build_csv_batches(tmp_path: Path, caplog: pytest.LogCaptureFixture, upl
 
 
 def test_build_csv_suffix_match(tmp_path: Path, upload_data: UploadData) -> None:
+    """
+    Test building CSV with matching read suffixes.
+
+    This test checks that an empty samples folder raises an error, and that the corresponding error message is
+    as expected.
+
+    Args:
+        tmp_path (Path): Temporary path for output files.
+        upload_data (UploadData): Data required for building the upload CSV.
+    """
     upload_data.illumina_read2_suffix = "_1.fastq.gz"
     with pytest.raises(ValueError) as e_info:
         build_upload_csv(
@@ -76,6 +119,16 @@ def test_build_csv_suffix_match(tmp_path: Path, upload_data: UploadData) -> None
 
 
 def test_build_csv_unmatched_files(tmp_path: Path, upload_data: UploadData) -> None:
+    """
+    Test building CSV with unmatched files.
+
+    This test checks that a samples folder with unmatched files raises an error, and that the corresponding
+    error message is as expected.
+
+    Args:
+        tmp_path (Path): Temporary path for output files.
+        upload_data (UploadData): Data required for building the upload CSV.
+    """
     with pytest.raises(ValueError) as e_info:
         build_upload_csv(
             "tests/data/unmatched_files",
@@ -86,7 +139,18 @@ def test_build_csv_unmatched_files(tmp_path: Path, upload_data: UploadData) -> N
 
 
 def test_build_csv_invalid_tech(tmp_path: Path, upload_data: UploadData) -> None:
-    # Note that this should be caught by the model validation
+    """
+    Test building CSV with an invalid instrument platform.
+
+    This test checks that an invalid instrument platform together with a samples folder with unmatched files
+    raises an error, and that the corresponding error message is as expected.
+
+    Note that this should be caught by the model validation.
+
+    Args:
+        tmp_path (Path): Temporary path for output files.
+        upload_data (UploadData): Data required for building the upload CSV.
+    """
     upload_data.instrument_platform = "invalid"
     with pytest.raises(ValueError) as e_info:
         build_upload_csv(
@@ -98,7 +162,12 @@ def test_build_csv_invalid_tech(tmp_path: Path, upload_data: UploadData) -> None
 
 
 def test_upload_data_model() -> None:
-    # Test that making model with invalid country makes error
+    """
+    Test the UploadData model validation.
+
+    This test ensures that the UploadData model raises validation errors for invalid data, such as platform, country and
+    specimen_organism.
+    """
     with pytest.raises(ValidationError):
         UploadData(
             batch_name="batch_name",
