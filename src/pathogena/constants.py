142a446eca5684fc727e128e80b4da4616ece9d5
Sarah Mapplebeck <sarah.mapplebeck@hippodigital.co.uk>
2025-02-03 15:31:43 +0000

    PR comments - retry uplaoding failing chunks

--- src/pathogena/constants.py
+++ src/pathogena/constants.py
@@ -16,17 +16,8 @@ DEFAULT_METADATA = {
     "illumina_read2_suffix": "_2.fastq.gz",
     "max_batch_size": 50,
 }
-DEFAULT_METADATA = {
-    "country": None,
-    "district": "",
-    "subdivision": "",
-    "instrument_platform": "illumina",
-    "pipeline": "mycobacteria",
-    "ont_read_suffix": ".fastq.gz",
-    "illumina_read1_suffix": "_1.fastq.gz",
-    "illumina_read2_suffix": "_2.fastq.gz",
-    "max_batch_size": 50,
-}
 HOSTILE_INDEX_NAME = "human-t2t-hla-argos985-mycob140"
 
 DEFAULT_CHUNK_SIZE = int(os.getenv("NEXT_PUBLIC_CHUNK_SIZE", 5000000))  # 5000000 = 5 mb
+DEFAULT_MAX_UPLOAD_RETRIES = int(os.getenv("MAX_UPLOAD_RETRIES", 5))
+DEFAULT_RETRY_DELAY = int(os.getenv("RETRY_DELAY", 2))
