a85982efb07501f44277fa5ed549c5628fc8b2fc
Jack Alexander <jack.alexander@hippodigital.co.uk>
2025-06-19 14:34:58 +0100

    chore: add info log line for each file upload

--- src/pathogena/upload_utils.py
+++ src/pathogena/upload_utils.py
@@ -635,11 +635,12 @@ def upload_files(
 
     # files have been sucessfully prepared, extract the prepared file list
     selected_files = file_preparation["files"]
-
+    
     # upload the file chunks
     with ThreadPoolExecutor(max_workers=upload_data.max_concurrent_chunks) as executor:
         futures = []
         for file in selected_files:
+            logging.info(f"Uploading {file['file']['name']}")
             future = executor.submit(upload_chunks, upload_data, file, file_status)
             futures.append(future)
 
